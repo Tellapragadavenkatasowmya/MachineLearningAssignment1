3f1c2e7a18a97ec932a9e0317e08056897552a7f
Tellapragadavenkatasowmya <112658897+Tellapragadavenkatasowmya@users.noreply.github.com>
2022-09-01 14:33:07 -0500

    Create eigthprogram.py

---
+++ eigthprogram.py
@@ -0,0 +1,6 @@
+# Taking radius as input from user 
+radius_of_circle=int(input("Please enter the radius = "))
+# printing the area of circle by using string formatting method and concatinating the values accordingly
+print("area ="+str(3.14)+"*"+str(radius_of_circle)+"** 2")
+area_of_circle=3.14*(radius_of_circle**2)
+print("The are of circle with radius "+str(radius_of_circle)+" is "+str(area_of_circle)+"meters square")
