9d9a82e0dbb80511b13052340c2ce0a189196fff
Tellapragadavenkatasowmya <112658897+Tellapragadavenkatasowmya@users.noreply.github.com>
2022-09-01 14:31:03 -0500

    Create fifthprogram.py

---
+++ fifthprogram.py
@@ -0,0 +1,9 @@
+#declaring pi(3.14) value to a variable named PI
+PI=3.14
+#Taking radius as input from users
+radius=float(input('please enter a value:'))
+#Calculating circumference and area of a circle by using formulas and assigning it to a variable
+_circum_of_circle_=2*radius
+_area_of_circle_=PI*radius*radius
+print("Circumference of a circle:",_circum_of_circle_)
+print("Area of a circle",_area_of_circle_)
